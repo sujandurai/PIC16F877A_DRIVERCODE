35a6ad8dc4ca6e4aa613d63d377e43a8122ce661
sujandurai <sujanduraisujan@gmail.com>
2026-02-19 22:05:40 +0530

    Update main.c

--- Embedded Driver/src/main.c
+++ Embedded Driver/src/main.c
@@ -1,1 +1,18 @@
 
+// example //#include"lcd.h"  we can include those library and work upon that based on our application
+
+#define _XTAL_FREQ 20000000
+#pragma config FOSC = HS
+#pragma config WDTE = OFF
+#pragma config PWRTE = OFF
+#pragma config BOREN = OFF
+#pragma config LVP = OFF
+#pragma config CPD = OFF
+#pragma config WRT = OFF
+#pragma config CP = OFF
+void main()
+{
+
+  // your projects or coding 
+   
+}
