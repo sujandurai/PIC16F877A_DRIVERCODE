567f4509eb7907086f3c139069a1a30a82658ece
sujandurai <sujanduraisujan@gmail.com>
2026-06-02 20:40:40 +0530

    Create UART_BITBANKING.C

---
+++ Embedded Driver/src/UART_BITBANKING.C
@@ -0,0 +1,23 @@
+
+#include "UART.H" 
+//#define _XTAL_FREQ 20000000
+
+#pragma config FOSC = HS        // High Speed Oscillator
+#pragma config WDTE = OFF       // Watchdog Timer disabled
+#pragma config PWRTE = OFF      // Power-up Timer disabled
+#pragma config BOREN = ON       // Brown-out Reset enabled
+#pragma config LVP = OFF        // Low-Voltage Programming disabled
+#pragma config CPD = OFF        // Data EEPROM protection off
+#pragma config WRT = OFF        // Flash Write protection off
+#pragma config CP = OFF         // Code protection off
+
+
+
+void main(void) 
+{
+    uart_init();
+    transmit("A");
+    string_transmit(" PIC16F877A ");
+            
+     receiver();  
+}
